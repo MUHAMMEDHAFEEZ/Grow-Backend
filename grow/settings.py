b793f3a68e32ea1ef76139b9ae8bce3c45c3605c
MUHAMMEDHAFEEZ <621172@oi.edu.eg>
2026-04-12 16:28:40 +0200

    feat: Update ALLOWED_HOSTS in settings for deployment

--- grow/settings.py
+++ grow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q45a-b97emvockz*0d50s!rqpeu2=+tk(xx5lyzrfosd+_v89=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["edugrow.pythonanywhere.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
