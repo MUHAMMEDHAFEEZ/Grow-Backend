93a63f9df9a5467298cbe9860ca6460e98b9051a
MUHAMMEDHAFEEZ <621172@oi.edu.eg>
2026-04-12 17:35:05 +0200

    fix: Move corsheaders middleware to the correct position in MIDDLEWARE settings

--- grow/settings.py
+++ grow/settings.py
@@ -55,10 +55,11 @@ INSTALLED_APPS = [
     'grades.apps.GradesConfig',
     'attendance.apps.AttendanceConfig',
     'notifications.apps.NotificationsConfig',
-    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'grow.urls'
