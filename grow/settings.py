c4a51f40f2a84d32126466a89b1b49fa60dbed96
ahmed_atris <aahhmmeed231185@gmail.com>
2026-05-17 19:11:12 +0300

    solve settings problem

--- grow/settings.py
+++ grow/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q45a-b97emvockz*0d50s!rqpeu2=+tk(xx5lyzrfosd+_v89="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # "edugrow.pythonanywhere.com", "localhost", "127.0.0.1",
 ALLOWED_HOSTS = ["*"]
 
